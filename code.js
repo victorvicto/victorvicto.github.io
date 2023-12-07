fc946d2ea371cbf16968aec41d229e62e2aa266e
Victor Demu <victor.demu@gmail.com>
2023-12-07 15:00:13 +0100

    Add files via upload

---
+++ code.js
@@ -0,0 +1,528 @@
+const lines_info = {
+    "001":{
+        "color_hex":"#B5378C",
+        "type": "m"
+    },
+    "002":{
+        "color_hex":"#ED6C23",
+        "type": "m"
+    },
+    "003":{
+        "color_hex":"#B4BD10",
+        "type": "t"
+    },
+    "004":{
+        "color_hex":"#EA4F80",
+        "type": "t"
+    },
+    "005":{
+        "color_hex":"#F6A90B",
+        "type": "m"
+    },
+    "006":{
+        "color_hex":"#0066A3",
+        "type": "m"
+    },
+    "007":{
+        "color_hex":"#EFE048",
+        "type": "t"
+    },
+    "008":{
+        "color_hex":"#169FDB",
+        "type": "t"
+    },
+    "009":{
+        "color_hex":"#C44F97",
+        "type": "t"
+    },
+    "012":{
+        "color_hex":"#4C8B33",
+        "type": "b"
+    },
+    "013":{
+        "color_hex":"#91BEE7",
+        "type": "b"
+    },
+    "014":{
+        "color_hex":"#F29DC3",
+        "type": "b"
+    },
+    "017":{
+        "color_hex":"#E43C2E",
+        "type": "b"
+    },
+    "018":{
+        "color_hex":"#9EBFE3",
+        "type": "t"
+    },
+    "019":{
+        "color_hex":"#E43C2E",
+        "type": "t"
+    },
+    "020":{
+        "color_hex":"#F3C300",
+        "type": "b"
+    },
+    "021":{
+        "color_hex":"#FFDC01",
+        "type": "b"
+    },
+    "025":{
+        "color_hex":"#A12944",
+        "type": "t"
+    },
+    "027":{
+        "color_hex":"#91BEE7",
+        "type": "b"
+    },
+    "028":{
+        "color_hex":"#E43C2E",
+        "type": "b"
+    },
+    "029":{
+        "color_hex":"#ED7807",
+        "type": "b"
+    },
+    "033":{
+        "color_hex":"#F29DC3",
+        "type": "b"
+    },
+    "034":{
+        "color_hex":"#F3C300",
+        "type": "b"
+    },
+    "036":{
+        "color_hex":"#91BEE7",
+        "type": "b"
+    },
+    "037":{
+        "color_hex":"#306196",
+        "type": "b"
+    },
+    "038":{
+        "color_hex":"#A67CB0",
+        "type": "b"
+    },
+    "039":{
+        "color_hex":"#E43C2E",
+        "type": "t"
+    },
+    "041":{
+        "color_hex":"#91BEE7",
+        "type": "b"
+    },
+    "042":{
+        "color_hex":"#4C8B33",
+        "type": "b"
+    },
+    "043":{
+        "color_hex":"#9B6018",
+        "type": "b"
+    },
+    "044":{
+        "color_hex":"#F3C300",
+        "type": "t"
+    },
+    "045":{
+        "color_hex":"#A67CB0",
+        "type": "b"
+    },
+    "046":{
+        "color_hex":"#E43C2E",
+        "type": "b"
+    },
+    "047":{
+        "color_hex":"#A67CB0",
+        "type": "b"
+    },
+    "048":{
+        "color_hex":"#ED7807",
+        "type": "b"
+    },
+    "049":{
+        "color_hex":"#306196",
+        "type": "b"
+    },
+    "050":{
+        "color_hex":"#B4BD10",
+        "type": "b"
+    },
+    "051":{
+        "color_hex":"#F3C300",
+        "type": "t"
+    },
+    "052":{
+        "color_hex":"#FFDC01",
+        "type": "b"
+    },
+    "053":{
+        "color_hex":"#4C8B33",
+        "type": "b"
+    },
+    "054":{
+        "color_hex":"#E43C2E",
+        "type": "b"
+    },
+    "055":{
+        "color_hex":"#F3C300",
+        "type": "t"
+    },
+    "056":{
+        "color_hex":"#ED7807",
+        "type": "b"
+    },
+    "057":{
+        "color_hex":"#E43C2E",
+        "type": "b"
+    },
+    "058":{
+        "color_hex":"#4C8B33",
+        "type": "b"
+    },
+    "059":{
+        "color_hex":"#9B6018",
+        "type": "b"
+    },
+    "060":{
+        "color_hex":"#F29DC3",
+        "type": "b"
+    },
+    "061":{
+        "color_hex":"#FFDC01",
+        "type": "b"
+    },
+    "062":{
+        "color_hex":"#F29DC3",
+        "type": "t"
+    },
+    "063":{
+        "color_hex":"#91BEE7",
+        "type": "b"
+    },
+    "064":{
+        "color_hex":"#E43C2E",
+        "type": "b"
+    },
+    "065":{
+        "color_hex":"#F3C300",
+        "type": "b"
+    },
+    "066":{
+        "color_hex":"#306196",
+        "type": "b"
+    },
+    "069":{
+        "color_hex":"#ED7807",
+        "type": "b"
+    },
+    "071":{
+        "color_hex":"#4C8B33",
+        "type": "b"
+    },
+    "072":{
+        "color_hex":"#F29DC3",
+        "type": "b"
+    },
+    "073":{
+        "color_hex":"#F29DC3",
+        "type": "b"
+    },
+    "074":{
+        "color_hex":"#A67CB0",
+        "type": "b"
+    },
+    "075":{
+        "color_hex":"#FFDC01",
+        "type": "b"
+    },
+    "076":{
+        "color_hex":"#FFDC01",
+        "type": "b"
+    },
+    "077":{
+        "color_hex":"#4C8B33",
+        "type": "b"
+    },
+    "078":{
+        "color_hex":"#A67CB0",
+        "type": "b"
+    },
+    "079":{
+        "color_hex":"#306196",
+        "type": "b"
+    },
+    "080":{
+        "color_hex":"#4C8B33",
+        "type": "b"
+    },
+    "081":{
+        "color_hex":"#4C8B33",
+        "type": "t"
+    },
+    "082":{
+        "color_hex":"#9EBFE3",
+        "type": "t"
+    },
+    "083":{
+        "color_hex":"#B4BD10",
+        "type": "b"
+    },
+    "086":{
+        "color_hex":"#306196",
+        "type": "b"
+    },
+    "087":{
+        "color_hex":"#4C8B33",
+        "type": "b"
+    },
+    "088":{
+        "color_hex":"#A12944",
+        "type": "b"
+    },
+    "089":{
+        "color_hex":"#B4BD10",
+        "type": "b"
+    },
+    "090":{
+        "color_hex":"#91BEE7",
+        "type": "b"
+    },
+    "092":{
+        "color_hex":"#E43C2E",
+        "type": "t"
+    },
+    "093":{
+        "color_hex":"#ED7807",
+        "type": "t"
+    },
+    "095":{
+        "color_hex":"#306196",
+        "type": "b"
+    },
+    "097":{
+        "color_hex":"#A12944",
+        "type": "t"
+    },
+    "119":{
+        "color_hex":"#E43C2E",
+        "type": "b"
+    },
+    "144":{
+        "color_hex":"#F3C300",
+        "type": "b"
+    },
+    "182":{
+        "color_hex":"#9EBFE3",
+        "type": "b"
+    },
+    "192":{
+        "color_hex":"#E43C2E",
+        "type": "b"
+    },
+    "204":{
+        "color_hex":"#C44F97",
+        "type": "b"
+    },
+    "205":{
+        "color_hex":"#A67CB0",
+        "type": "b"
+    },
+    "206":{
+        "color_hex":"#169FDB",
+        "type": "b"
+    },
+    "208":{
+        "color_hex":"#91BEE7",
+        "type": "b"
+    },
+    "209":{
+        "color_hex":"#C44F97",
+        "type": "b"
+    },
+    "210":{
+        "color_hex":"#C1D66B",
+        "type": "b"
+    },
+    "211":{
+        "color_hex":"#9B6018",
+        "type": "b"
+    },
+    "212":{
+        "color_hex":"#ED7807",
+        "type": "b"
+    },
+    "213":{
+        "color_hex":"#A12944",
+        "type": "b"
+    },
+    "216":{
+        "color_hex":"#74C095",
+        "type": "b"
+    },
+    "218":{
+        "color_hex":"#23A845",
+        "type": "b"
+    },
+    "240":{
+        "color_hex":"#F3C300",
+        "type": "b"
+    }
+}
+let stops = [];
+let time_out_id = -1;
+let current_salve = "even";
+const stop_container = document.getElementById("stops-container");
+let error_message_h2 = document.getElementById("error-message");
+let stib_api_key = localStorage.getItem("stib_api_key");
+if(!stib_api_key){
+    stib_api_key = prompt("Please enter your STIB API key");
+    localStorage.setItem("stib_api_key", stib_api_key);
+}
+
+function WhiteOrBlack(hex_code){
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex_code);
+    let R = parseInt(result[1], 16);
+    let G = parseInt(result[2], 16);
+    let B = parseInt(result[3], 16);
+    let L = 0.2126 * R + 0.7152 * G + 0.0722 * B;
+    if(L<200){
+        return "white";
+    } else {
+        return "black";
+    }
+}
+
+function GetTimeInfo(time_string){
+    let date = new Date(time_string);
+    let minutes_delta = Math.floor((date - new Date())/60000);
+    if(minutes_delta<=0){
+        return "↓↓";
+    }
+    return minutes_delta.toString();
+}
+
+function CreateDepartureParagraph(departure, classname, stop_div){
+    let line_key = departure.lineId.padStart(3, '0');
+    if(!(line_key in lines_info)) return;
+    let dep_p = document.createElement("p");
+    let time_info = GetTimeInfo(departure.expectedArrivalTime);
+    dep_p.setAttribute("dep", departure.expectedArrivalTime);
+    dep_p.setAttribute("salve", current_salve);
+    dep_p.className = classname;
+    let line = "<span>";
+    if(lines_info[line_key]["type"]=="b"){
+        line += "<i class='fa-solid fa-bus'></i> ";
+    } else if(lines_info[line_key]["type"]=="t"){
+        line += "<i class='fa-solid fa-train-tram'></i> ";
+    } else if(lines_info[line_key]["type"]=="m"){
+        line += "<i class='fa-solid fa-train-subway'></i> ";
+    }
+    line += "<span class='line-num' style='background-color:"+lines_info[line_key]["color_hex"]+";color:"+WhiteOrBlack(lines_info[line_key]["color_hex"])+";'>"+departure.lineId+"</span> ";
+    line += departure.destination.fr+"</span>";
+    line += "<b class='time-info'>"+time_info+"</b>";
+    dep_p.innerHTML = line;
+    stop_div.appendChild(dep_p);
+}
+
+function DeleteUnchangedDepartures(){
+    let deps = document.getElementsByTagName("p");
+    for(let i=deps.length-1; i>=0; i--){
+        if(deps[i].getAttribute("salve")!=current_salve) deps[i].remove();
+    }
+}
+
+function ManageDeparture(departure, stop_name, stop_div){
+    let classname = stop_name+"_"+departure.lineId+"_"+departure.destination.fr;
+    let already_existing = document.getElementsByClassName(classname);
+    let added = false;
+    for(let p of already_existing){
+        if(p.getAttribute("salve")!=current_salve){
+            p.getElementsByClassName("time-info")[0].innerText = GetTimeInfo(departure.expectedArrivalTime);
+            p.setAttribute("salve", current_salve);
+            p.setAttribute("dep", departure.expectedArrivalTime);
+            added = true;
+            break;
+        }
+    }
+    if(!added){
+        CreateDepartureParagraph(departure, classname, stop_div);
+    }
+}
+
+async function AddStop(stop){
+    if(stop.stop_id.includes("G") || stop.stop_id.includes("F") || stop.stop_id.includes("B")) return;
+    let stop_div = document.getElementById(stop.stop_name);
+    let needs_adding = false;
+    if (!stop_div){
+        stop_div = document.createElement("div");
+        stop_div.className = "stop";
+        stop_div.id = stop.stop_name;
+        let stop_title = document.createElement("h1");
+        stop_title.innerText = stop.stop_name;
+        stop_div.appendChild(stop_title);
+        needs_adding = true;
+    }
+
+    let departures_response = await fetch("https://stibmivb.opendatasoft.com/api/explore/v2.1/catalog/datasets/waiting-time-rt-production/records?apikey="+stib_api_key+"&where=pointid%3D"+stop.stop_id+"&limit=2");
+    const departures = await departures_response.json();
+    if(departures.total_count>0){
+        for(let line of departures.results){
+            for (let departure of JSON.parse(line.passingtimes)){
+                ManageDeparture(departure, stop.stop_name, stop_div);
+            }
+        }
+        if(needs_adding){
+            stop_container.appendChild(stop_div);
+        }
+        let children = [...stop_div.getElementsByTagName("p")];
+        for(let c of children){
+            stop_div.removeChild(c);
+        }
+        children.sort((a, b) => a.getAttribute("dep").localeCompare(b.getAttribute("dep")));
+        stop_div.append(...children);
+    }
+}
+
+function check_live(){
+    if(document.getElementById("live-input").checked){
+        load();
+    } else {
+        if(time_out_id>-1){
+            clearTimeout(time_out_id);
+            time_out_id = -1;
+        }
+    }
+}
+
+function startup_location(){
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(async function(position) {
+            const latitude = position.coords.latitude;
+            const longitude = position.coords.longitude;
+            let distance = document.getElementById("distance-input").value + "m";
+            console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
+            const stops_response = await fetch("https://stibmivb.opendatasoft.com/api/explore/v2.1/catalog/datasets/gtfs-stops-production/records?apikey="+stib_api_key+"&where=within_distance(stop_coordinates%2C%20GEOM%27%7B%22type%22%3A%20%22Point%22%2C%22coordinates%22%3A%20%5B"+longitude+"%2C%20"+latitude+"%5D%7D%27%2C%20"+distance+")&limit=20");
+            stops = await stops_response.json();
+            load();
+        });
+    } else {
+        error_message_h2.innerText = "Navigator doesn't support geolocalistation";
+    }
+}
+
+async function load(){
+    console.log("reloading...");
+    current_salve = (current_salve=="even") ? "odd" : "even";
+    if(stops.total_count>0){
+        for (let stop of stops.results){
+            await AddStop(stop);
+        }
+    } else {
+        error_message_h2.innerText = "No stops found nearby...";
+    }
+    DeleteUnchangedDepartures();
+    if(document.getElementById("live-input").checked) time_out_id = setTimeout(load, 20000);
+    console.log("reload finished!");
+}
+
+startup_location();
